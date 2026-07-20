bcc98aae4374c8f48ea508e7e6517db43bfe788e
morteega <marcos.ortega@alumnos.upm.es>
2026-07-20 17:04:18 +0200

    Changes to telegram_bot done

--- telegram_bot/config.py
+++ telegram_bot/config.py
@@ -1,20 +1,16 @@
 import os
 from pathlib import Path
-
 from dotenv import load_dotenv
 
 
 BASE_DIR = Path(__file__).resolve().parent
-ENV_PATH = BASE_DIR / ".env"
-
 load_dotenv(ENV_PATH)
 
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID", "")
-BACKEND_URL = os.getenv("BACKEND_URL", "http://localhost:8080")
+BACKEND_URL = os.getenv("BACKEND_URL", "http:192.168.50.1:5000")
 
 AUTHORIZED_USERS_RAW = os.getenv("AUTHORIZED_USERS", "")
-
 AUTHORIZED_USERS = [
     int(user_id.strip())
     for user_id in AUTHORIZED_USERS_RAW.split(",")
